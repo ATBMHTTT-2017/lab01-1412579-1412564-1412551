3e1c4525ae26540f8112425f2d8e2adebc9495b3
1412579 - Vũ Minh Trí <1412579@student.hcmus.edu.vn>
2017-05-13 02:30:52 +0700

    update create data and user

--- 1412579-schema.sql
+++ 1412579-schema.sql
@@ -1,29 +1,29 @@
-drop table NhanVien
-drop table ChiNhanh
-drop table ChiTieu
-drop table PhanCong
-drop table Phongban
-drop table DuAn
+drop table NhanVien;
+drop table ChiNhanh;
+drop table ChiTieu;
+drop table PhanCong;
+drop table Phongban;
+drop table DuAn;
 
 
 Create table NhanVien(
-maNV number GENERATED ALWAYS as IDENTITY(START with 10000 INCREMENT by 1),
+maNV number GENERATED ALWAYS as IDENTITY(START with 99999 INCREMENT by 1),
 hoTen varchar2(50) NOT NULL,
 diaChi varchar2(100) NOT NULL,
 dienThoai number,
 email varchar2(100),
 maPhong number,
 chiNhanh number,
-luong number(10,2),
-chucVu numer(1,0)
-)
+luong number(20,2),
+chucVu number(1,0)
+);
 --Chuc vu 0 là nhân viên, 1 là giám đống
 
 Create table ChiNhanh(
 maCN number GENERATED ALWAYS as IDENTITY(START with 1 INCREMENT by 1),
 tenCN varchar2(50) NOT NULL,
 truongChiNhanh number NOT NULL
-)
+);
 
 
 Create table PhongBan(
@@ -33,114 +33,114 @@ truongPhong number NOT NULL,
 ngayNhanChuc date,
 soNhanVien number,
 chiNhanh number NOT NULL
-)
+);
 
 Create table  DuAn(
 maDA number GENERATED ALWAYS as IDENTITY(START with 1 INCREMENT by 1),
 tenDA varchar2(50) NOT NULL,
-kinhPhi number(10,2),
+kinhPhi number(20,2),
 phongChuTri number,
 truongDA number
-)
+);
 
 Create table ChiTieu(
 maChiTieu number GENERATED ALWAYS as IDENTITY(START with 1 INCREMENT by 1),
 tenChiTieu varchar2(50) NOT NULL,
-soTien number(10,2),
+soTien number(20,2),
 duAn number NOT NULL
-)
+);
 
 
 Create table PhanCong(
 maNV number,
 duAn number,
 vaiTro varchar2(20),
-phuCap number(10,2)
-)
+phuCap number(20,2)
+);
 
 -------------------------------------------------
 -- PRIMARY KEY
 ALTER TABLE PhanCong
 ADD CONSTRAINT PK_PhanCong
-PRIMARY KEY(maNV, duAn)
+PRIMARY KEY(maNV, duAn);
 
 ALTER TABLE ChiNhanh
 ADD CONSTRAINT PK_ChiNhanh
-PRIMARY KEY(maCN)
+PRIMARY KEY(maCN);
 
-ALTER TABLE NhanVie
+ALTER TABLE NhanVien
 ADD CONSTRAINT PK_NhanVien
-PRIMARY KEY(maNV)
+PRIMARY KEY(maNV);
 
 ALTER TABLE PhongBan
 ADD CONSTRAINT PK_PhongBan
-PRIMARY KEY(maPhong)
+PRIMARY KEY(maPhong);
 
 ALTER TABLE DuAn
 ADD CONSTRAINT PK_DuAn
-PRIMARY KEY(maDA)
+PRIMARY KEY(maDA);
 
 ALTER TABLE ChiTieu
 ADD CONSTRAINT PK_ChiTieu
-PRIMARY KEY(maChiTieu)
+PRIMARY KEY(maChiTieu);
 
 ------------------------------------------------
 --FK CHI NHÁNH - NHÂN VIÊN
 ALTER TABLE ChiNhanh
 ADD CONSTRAINT FK1_ChiNhanh_NhanVien 
 FOREIGN KEY(truongChiNhanh) 
-REFERENCES NhanVien(maNV)
+REFERENCES NhanVien(maNV);
 
 --FK NHÂN VIÊN - PHÒNG BAN
 ALTER TABLE NhanVien
 ADD CONSTRAINT FK1_NhanVien_PhongBan 
 FOREIGN KEY(maPhong) 
-REFERENCES PhongBan(maPhong)
+REFERENCES PhongBan(maPhong);
 
 -- FK NHÂN VIÊN - CHI NHÁNH 
 ALTER TABLE NhanVien
 ADD CONSTRAINT FK2_NhanVien_ChiNhanh 
 FOREIGN KEY(chiNhanh) 
-REFERENCES ChiNhanh(maCN)
+REFERENCES ChiNhanh(maCN);
 
 -- FK PHÒNG BAN
 ALTER TABLE PhongBan
 ADD CONSTRAINT FK1_PhongBan_ChiNhanh
 FOREIGN KEY(chiNhanh) 
-REFERENCES ChiNhanh(maCN)
+REFERENCES ChiNhanh(maCN);
 
 ALTER TABLE PhongBan
 ADD CONSTRAINT FK2_PhongBan_NhanVien 
 FOREIGN KEY(truongPhong) 
-REFERENCES NhanVien(maNV)
+REFERENCES NhanVien(maNV);
 
 -- FK DỰ DÁN
 ALTER TABLE DuAn
 ADD CONSTRAINT FK1_DuAn_NhanVien
 FOREIGN KEY(truongDA) 
-REFERENCES NhanVien(maNV)
+REFERENCES NhanVien(maNV);
 
 ALTER TABLE DuAn
 ADD CONSTRAINT FK2_DuAn_PhongBan
 FOREIGN KEY(PhongChuTri) 
-REFERENCES PhongBan(maPhong)
+REFERENCES PhongBan(maPhong);
 
 -- FK CHỈ TIÊU
 ALTER TABLE ChiTieu
 ADD CONSTRAINT FK1_ChiTieu_DuAn 
 FOREIGN KEY(duAn) 
-REFERENCES DuAn(maDA)
+REFERENCES DuAn(maDA);
 
 -- FK PHÂN CÔNG
 ALTER TABLE PhanCong
 ADD CONSTRAINT FK1_PhanCong_DuAn 
 FOREIGN KEY(duAn) 
-REFERENCES DuAn(maDA)
+REFERENCES DuAn(maDA);
 
 ALTER TABLE PhanCong
 ADD CONSTRAINT FK2_PhanCong_NhanVien
 FOREIGN KEY(maNV) 
-REFERENCES NhanVien(maNV)
+REFERENCES NhanVien(maNV);
 
 -- DELETE FK TO DROP TABLE
 alter table NhanVien drop constraint FK1_NhanVien_PhongBan
