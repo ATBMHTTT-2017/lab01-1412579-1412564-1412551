1971ec08e007848c2298ce3564c4e773be4b5a9d
1412579 - Vũ Minh Trí <1412579@student.hcmus.edu.vn>
2017-05-19 22:21:36 +0700

    update OLS 1

--- 1412579_OLS_1.sql
+++ 1412579_OLS_1.sql
@@ -123,8 +123,217 @@ END;
 
 --gán chính sách cho bảng dự án
 EXEC SA_POLICY_ADMIN.APPLY_TABLE_POLICY('OLS_DuAn', 'tangliang', 'DUAN', 'NO_CONTROL');
+EXEC SA_POLICY_ADMIN.REMOVE_TABLE_POLICY('OLS_DuAn', 'tangliang', 'DUAN');
 
+-- Grant quyền cho các trưởng phòng (NS,KT,KH) ở các chi nhánh khác nhau (7 chi nhánh x 3 phòng)
+grant select, update, insert on duan to "10001";
+grant select, update, insert on duan to "10002";
+grant select, update, insert on duan to "10003";
+grant select, update, insert on duan to "10004";
+grant select, update, insert on duan to "10005";
+grant select, update, insert on duan to "10027";
+grant select, update, insert on duan to "10028";
+grant select, update, insert on duan to "10029";
+grant select, update, insert on duan to "10030";
+grant select, update, insert on duan to "10031";
+grant select, update, insert on duan to "10032";
+grant select, update, insert on duan to "10033";
+grant select, update, insert on duan to "10034";
+grant select, update, insert on duan to "10035";
+grant select, update, insert on duan to "10036";
+grant select, update, insert on duan to "10037";
+grant select, update, insert on duan to "10038";
+grant select, update, insert on duan to "10039";
+grant select, update, insert on duan to "10040";
+grant select, update, insert on duan to "10041";
+grant select, update, insert on duan to "10042";
+grant select, update, insert on duan to "10043";
+grant select, update, insert on duan to "10044";
 
+-- Trưởng phòng được phép đọc dữ liệu dự án của tất cả phòng ban nhưng chỉ được phép ghi dữ liệu dự án thuộc phòng của mình.
 
+--Gán nhãn theo các phòng thuộc các chi nhánh
+--chi nhánh HCM
+update tangliang.DUAN
+set OLS_Col_DuAn = CHAR_TO_LABEL('OLS_DuAn', 'GH:NS:HCM')
+where phongChuTri = 2;
 
+update tangliang.DUAN
+set OLS_Col_DuAn = CHAR_TO_LABEL('OLS_DuAn', 'GH:KT:HCM')
+where phongChuTri = 3;
+
+update tangliang.DUAN
+set OLS_Col_DuAn = CHAR_TO_LABEL('OLS_DuAn', 'GH:KH:HCM')
+where phongChuTri = 4;
+
+--chi nhánh HN
+update tangliang.DUAN
+set OLS_Col_DuAn = CHAR_TO_LABEL('OLS_DuAn', 'GH:NS:HN')
+where phongChuTri = 6;
+
+update tangliang.DUAN
+set OLS_Col_DuAn = CHAR_TO_LABEL('OLS_DuAn', 'GH:KT:HN')
+where phongChuTri = 7;
+
+update tangliang.DUAN
+set OLS_Col_DuAn = CHAR_TO_LABEL('OLS_DuAn', 'GH:KH:HN')
+where phongChuTri = 8;
+
+--chi nhánh DN
+update tangliang.DUAN
+set OLS_Col_DuAn = CHAR_TO_LABEL('OLS_DuAn', 'GH:NS:DN')
+where phongChuTri = 9;
+
+update tangliang.DUAN
+set OLS_Col_DuAn = CHAR_TO_LABEL('OLS_DuAn', 'GH:KT:DN')
+where phongChuTri = 10;
+
+update tangliang.DUAN
+set OLS_Col_DuAn = CHAR_TO_LABEL('OLS_DuAn', 'GH:KH:DN')
+where phongChuTri = 11;
+
+--chi nhánh VT
+update tangliang.DUAN
+set OLS_Col_DuAn = CHAR_TO_LABEL('OLS_DuAn', 'GH:NS:VT')
+where phongChuTri = 12;
+
+update tangliang.DUAN
+set OLS_Col_DuAn = CHAR_TO_LABEL('OLS_DuAn', 'GH:KT:VT')
+where phongChuTri = 13;
+
+update tangliang.DUAN
+set OLS_Col_DuAn = CHAR_TO_LABEL('OLS_DuAn', 'GH:KH:VT')
+where phongChuTri = 14;
+
+--chi nhánh DNG
+update tangliang.DUAN
+set OLS_Col_DuAn = CHAR_TO_LABEL('OLS_DuAn', 'GH:NS:DNG')
+where phongChuTri = 15;
+
+update tangliang.DUAN
+set OLS_Col_DuAn = CHAR_TO_LABEL('OLS_DuAn', 'GH:KT:DNG')
+where phongChuTri = 16;
+
+update tangliang.DUAN
+set OLS_Col_DuAn = CHAR_TO_LABEL('OLS_DuAn', 'GH:KH:DNG')
+where phongChuTri = 17;
+
+--chi nhánh SEO
+update tangliang.DUAN
+set OLS_Col_DuAn = CHAR_TO_LABEL('OLS_DuAn', 'GH:NS:SEO')
+where phongChuTri = 18;
+
+update tangliang.DUAN
+set OLS_Col_DuAn = CHAR_TO_LABEL('OLS_DuAn', 'GH:KT:SEO')
+where phongChuTri = 19;
+
+update tangliang.DUAN
+set OLS_Col_DuAn = CHAR_TO_LABEL('OLS_DuAn', 'GH:KH:SEO')
+where phongChuTri = 20;
+
+--chi nhánh HK
+update tangliang.DUAN
+set OLS_Col_DuAn = CHAR_TO_LABEL('OLS_DuAn', 'GH:NS:HK')
+where phongChuTri = 21;
+
+update tangliang.DUAN
+set OLS_Col_DuAn = CHAR_TO_LABEL('OLS_DuAn', 'GH:KT:HK')
+where phongChuTri = 22;
+
+update tangliang.DUAN
+set OLS_Col_DuAn = CHAR_TO_LABEL('OLS_DuAn', 'GH:KH:HK')
+where phongChuTri = 23;
+
+-- gán level cho user trưởng phòng của các phòng thuộc chi nhánh
+EXEC SA_USER_ADMIN.SET_LEVELS ('OLS_DuAn', '10002', 'BM', 'GH', 'BM', 'BM');
+EXEC SA_USER_ADMIN.SET_LEVELS ('OLS_DuAn', '10003', 'BM', 'GH', 'BM', 'BM');
+EXEC SA_USER_ADMIN.SET_LEVELS ('OLS_DuAn', '10004', 'BM', 'GH', 'BM', 'BM');
+
+EXEC SA_USER_ADMIN.SET_LEVELS ('OLS_DuAn', '10027', 'BM', 'GH', 'BM', 'BM');
+EXEC SA_USER_ADMIN.SET_LEVELS ('OLS_DuAn', '10028', 'BM', 'GH', 'BM', 'BM');
+EXEC SA_USER_ADMIN.SET_LEVELS ('OLS_DuAn', '10029', 'BM', 'GH', 'BM', 'BM');
+
+EXEC SA_USER_ADMIN.SET_LEVELS ('OLS_DuAn', '10030', 'BM', 'GH', 'BM', 'BM');
+EXEC SA_USER_ADMIN.SET_LEVELS ('OLS_DuAn', '10031', 'BM', 'GH', 'BM', 'BM');
+EXEC SA_USER_ADMIN.SET_LEVELS ('OLS_DuAn', '10032', 'BM', 'GH', 'BM', 'BM');
+EXEC SA_USER_ADMIN.SET_LEVELS ('OLS_DuAn', '10033', 'BM', 'GH', 'BM', 'BM');
+EXEC SA_USER_ADMIN.SET_LEVELS ('OLS_DuAn', '10034', 'BM', 'GH', 'BM', 'BM');
+EXEC SA_USER_ADMIN.SET_LEVELS ('OLS_DuAn', '10035', 'BM', 'GH', 'BM', 'BM');
+EXEC SA_USER_ADMIN.SET_LEVELS ('OLS_DuAn', '10036', 'BM', 'GH', 'BM', 'BM');
+EXEC SA_USER_ADMIN.SET_LEVELS ('OLS_DuAn', '10037', 'BM', 'GH', 'BM', 'BM');
+EXEC SA_USER_ADMIN.SET_LEVELS ('OLS_DuAn', '10038', 'BM', 'GH', 'BM', 'BM');
+EXEC SA_USER_ADMIN.SET_LEVELS ('OLS_DuAn', '10039', 'BM', 'GH', 'BM', 'BM');
+
+EXEC SA_USER_ADMIN.SET_LEVELS ('OLS_DuAn', '10040', 'BM', 'GH', 'BM', 'BM');
+EXEC SA_USER_ADMIN.SET_LEVELS ('OLS_DuAn', '10041', 'BM', 'GH', 'BM', 'BM');
+EXEC SA_USER_ADMIN.SET_LEVELS ('OLS_DuAn', '10042', 'BM', 'GH', 'BM', 'BM');
+EXEC SA_USER_ADMIN.SET_LEVELS ('OLS_DuAn', '10043', 'BM', 'GH', 'BM', 'BM');
+EXEC SA_USER_ADMIN.SET_LEVELS ('OLS_DuAn', '10044', 'BM', 'GH', 'BM', 'BM');
+
+--Gán compartment cho user trưởng phòng. 
+--(Mỗi thằng trưởng phòng chỉ được đọc các dự án phòng khác, và chỉ được ghi phòng mình)
+--Phòng nhân sự
+EXEC SA_USER_ADMIN.SET_COMPARTMENTS(policy_name => 'OLS_DuAn',user_name => '10002',read_comps => 'NS,KT,KH',write_comps => 'NS',def_comps => 'NS',row_comps => 'NS');
+EXEC SA_USER_ADMIN.SET_COMPARTMENTS(policy_name => 'OLS_DuAn',user_name => '10027',read_comps => 'NS,KT,KH',write_comps => 'NS',def_comps => 'NS',row_comps => 'NS');
+EXEC SA_USER_ADMIN.SET_COMPARTMENTS(policy_name => 'OLS_DuAn',user_name => '10030',read_comps => 'NS,KT,KH',write_comps => 'NS',def_comps => 'NS',row_comps => 'NS');
+EXEC SA_USER_ADMIN.SET_COMPARTMENTS(policy_name => 'OLS_DuAn',user_name => '10033',read_comps => 'NS,KT,KH',write_comps => 'NS',def_comps => 'NS',row_comps => 'NS');
+EXEC SA_USER_ADMIN.SET_COMPARTMENTS(policy_name => 'OLS_DuAn',user_name => '10036',read_comps => 'NS,KT,KH',write_comps => 'NS',def_comps => 'NS',row_comps => 'NS');
+EXEC SA_USER_ADMIN.SET_COMPARTMENTS(policy_name => 'OLS_DuAn',user_name => '10039',read_comps => 'NS,KT,KH',write_comps => 'NS',def_comps => 'NS',row_comps => 'NS');
+EXEC SA_USER_ADMIN.SET_COMPARTMENTS(policy_name => 'OLS_DuAn',user_name => '10042',read_comps => 'NS,KT,KH',write_comps => 'NS',def_comps => 'NS',row_comps => 'NS');
+--phòng kế toán
+EXEC SA_USER_ADMIN.SET_COMPARTMENTS(policy_name => 'OLS_DuAn',user_name => '10003',read_comps => 'NS,KT,KH',write_comps => 'KT',def_comps => 'KT',row_comps => 'KT');
+EXEC SA_USER_ADMIN.SET_COMPARTMENTS(policy_name => 'OLS_DuAn',user_name => '10028',read_comps => 'NS,KT,KH',write_comps => 'KT',def_comps => 'KT',row_comps => 'KT');
+EXEC SA_USER_ADMIN.SET_COMPARTMENTS(policy_name => 'OLS_DuAn',user_name => '10031',read_comps => 'NS,KT,KH',write_comps => 'KT',def_comps => 'KT',row_comps => 'KT');
+EXEC SA_USER_ADMIN.SET_COMPARTMENTS(policy_name => 'OLS_DuAn',user_name => '10034',read_comps => 'NS,KT,KH',write_comps => 'KT',def_comps => 'KT',row_comps => 'KT');
+EXEC SA_USER_ADMIN.SET_COMPARTMENTS(policy_name => 'OLS_DuAn',user_name => '10037',read_comps => 'NS,KT,KH',write_comps => 'KT',def_comps => 'KT',row_comps => 'KT');
+EXEC SA_USER_ADMIN.SET_COMPARTMENTS(policy_name => 'OLS_DuAn',user_name => '10040',read_comps => 'NS,KT,KH',write_comps => 'KT',def_comps => 'KT',row_comps => 'KT');
+EXEC SA_USER_ADMIN.SET_COMPARTMENTS(policy_name => 'OLS_DuAn',user_name => '10043',read_comps => 'NS,KT,KH',write_comps => 'KT',def_comps => 'KT',row_comps => 'KT');
+--phòng kế hoạch
+EXEC SA_USER_ADMIN.SET_COMPARTMENTS(policy_name => 'OLS_DuAn',user_name => '10004',read_comps => 'NS,KT,KH',write_comps => 'KH',def_comps => 'KH',row_comps => 'KH');
+EXEC SA_USER_ADMIN.SET_COMPARTMENTS(policy_name => 'OLS_DuAn',user_name => '10029',read_comps => 'NS,KT,KH',write_comps => 'KH',def_comps => 'KH',row_comps => 'KH');
+EXEC SA_USER_ADMIN.SET_COMPARTMENTS(policy_name => 'OLS_DuAn',user_name => '10032',read_comps => 'NS,KT,KH',write_comps => 'KH',def_comps => 'KH',row_comps => 'KH');
+EXEC SA_USER_ADMIN.SET_COMPARTMENTS(policy_name => 'OLS_DuAn',user_name => '10035',read_comps => 'NS,KT,KH',write_comps => 'KH',def_comps => 'KH',row_comps => 'KH');
+EXEC SA_USER_ADMIN.SET_COMPARTMENTS(policy_name => 'OLS_DuAn',user_name => '10038',read_comps => 'NS,KT,KH',write_comps => 'KH',def_comps => 'KH',row_comps => 'KH');
+EXEC SA_USER_ADMIN.SET_COMPARTMENTS(policy_name => 'OLS_DuAn',user_name => '10041',read_comps => 'NS,KT,KH',write_comps => 'KH',def_comps => 'KH',row_comps => 'KH');
+EXEC SA_USER_ADMIN.SET_COMPARTMENTS(policy_name => 'OLS_DuAn',user_name => '10044',read_comps => 'NS,KT,KH',write_comps => 'KH',def_comps => 'KH',row_comps => 'KH');
+
+--gán group cho các user
+exec sa_user_admin.set_groups(policy_name => 'OLS_DuAn',user_name => '10002',read_groups => 'HCM,HN,DN,VT,DNG,SEO,HK',write_groups => 'HCM',def_groups => 'HCM',row_groups => 'HCM');
+exec sa_user_admin.set_groups(policy_name => 'OLS_DuAn',user_name => '10003',read_groups => 'HCM,HN,DN,VT,DNG,SEO,HK',write_groups => 'HCM',def_groups => 'HCM',row_groups => 'HCM');
+exec sa_user_admin.set_groups(policy_name => 'OLS_DuAn',user_name => '10004',read_groups => 'HCM,HN,DN,VT,DNG,SEO,HK',write_groups => 'HCM',def_groups => 'HCM',row_groups => 'HCM');
+
+exec sa_user_admin.set_groups(policy_name => 'OLS_DuAn',user_name => '10027',read_groups => 'HCM,HN,DN,VT,DNG,SEO,HK',write_groups => 'HN',def_groups => 'HN',row_groups => 'HN');
+exec sa_user_admin.set_groups(policy_name => 'OLS_DuAn',user_name => '10028',read_groups => 'HCM,HN,DN,VT,DNG,SEO,HK',write_groups => 'HN',def_groups => 'HN',row_groups => 'HN');
+exec sa_user_admin.set_groups(policy_name => 'OLS_DuAn',user_name => '10029',read_groups => 'HCM,HN,DN,VT,DNG,SEO,HK',write_groups => 'HN',def_groups => 'HN',row_groups => 'HN');
+
+exec sa_user_admin.set_groups(policy_name => 'OLS_DuAn',user_name => '10030',read_groups => 'HCM,HN,DN,VT,DNG,SEO,HK',write_groups => 'DN',def_groups => 'DN',row_groups => 'DN');
+exec sa_user_admin.set_groups(policy_name => 'OLS_DuAn',user_name => '10031',read_groups => 'HCM,HN,DN,VT,DNG,SEO,HK',write_groups => 'DN',def_groups => 'DN',row_groups => 'DN');
+exec sa_user_admin.set_groups(policy_name => 'OLS_DuAn',user_name => '10032',read_groups => 'HCM,HN,DN,VT,DNG,SEO,HK',write_groups => 'DN',def_groups => 'DN',row_groups => 'DN');
+
+exec sa_user_admin.set_groups(policy_name => 'OLS_DuAn',user_name => '10033',read_groups => 'HCM,HN,DN,VT,DNG,SEO,HK',write_groups => 'VT',def_groups => 'VT',row_groups => 'VT');
+exec sa_user_admin.set_groups(policy_name => 'OLS_DuAn',user_name => '10034',read_groups => 'HCM,HN,DN,VT,DNG,SEO,HK',write_groups => 'VT',def_groups => 'VT',row_groups => 'VT');
+exec sa_user_admin.set_groups(policy_name => 'OLS_DuAn',user_name => '10035',read_groups => 'HCM,HN,DN,VT,DNG,SEO,HK',write_groups => 'VT',def_groups => 'VT',row_groups => 'VT');
+
+exec sa_user_admin.set_groups(policy_name => 'OLS_DuAn',user_name => '10036',read_groups => 'HCM,HN,DN,VT,DNG,SEO,HK',write_groups => 'DNG',def_groups => 'DNG',row_groups => 'DNG');
+exec sa_user_admin.set_groups(policy_name => 'OLS_DuAn',user_name => '10037',read_groups => 'HCM,HN,DN,VT,DNG,SEO,HK',write_groups => 'DNG',def_groups => 'DNG',row_groups => 'DNG');
+exec sa_user_admin.set_groups(policy_name => 'OLS_DuAn',user_name => '10038',read_groups => 'HCM,HN,DN,VT,DNG,SEO,HK',write_groups => 'DNG',def_groups => 'DNG',row_groups => 'DNG');
+
+exec sa_user_admin.set_groups(policy_name => 'OLS_DuAn',user_name => '10039',read_groups => 'HCM,HN,DN,VT,DNG,SEO,HK',write_groups => 'SEO',def_groups => 'SEO',row_groups => 'SEO');
+exec sa_user_admin.set_groups(policy_name => 'OLS_DuAn',user_name => '10040',read_groups => 'HCM,HN,DN,VT,DNG,SEO,HK',write_groups => 'SEO',def_groups => 'SEO',row_groups => 'SEO');
+exec sa_user_admin.set_groups(policy_name => 'OLS_DuAn',user_name => '10041',read_groups => 'HCM,HN,DN,VT,DNG,SEO,HK',write_groups => 'SEO',def_groups => 'SEO',row_groups => 'SEO');
+
+exec sa_user_admin.set_groups(policy_name => 'OLS_DuAn',user_name => '10042',read_groups => 'HCM,HN,DN,VT,DNG,SEO,HK',write_groups => 'HK',def_groups => 'HK',row_groups => 'HK');
+exec sa_user_admin.set_groups(policy_name => 'OLS_DuAn',user_name => '10043',read_groups => 'HCM,HN,DN,VT,DNG,SEO,HK',write_groups => 'HK',def_groups => 'HK',row_groups => 'HK');
+exec sa_user_admin.set_groups(policy_name => 'OLS_DuAn',user_name => '10044',read_groups => 'HCM,HN,DN,VT,DNG,SEO,HK',write_groups => 'HK',def_groups => 'HK',row_groups => 'HK');
+
+--Kiểm tra
+select SA_SESSION.MIN_LEVEL('OLS_DuAn') from dual;
+select SA_SESSION.MAX_LEVEL('OLS_DuAn') from dual;
+select SA_SESSION.COMP_READ('OLS_DuAn') from dual;
+select SA_SESSION.COMP_WRITE('OLS_DuAn') from dual;
+select SA_SESSION.GROUP_READ('OLS_DuAn') from dual;
+select SA_SESSION.GROUP_WRITE('OLS_DuAn') from dual;
+select SA_SESSION.LABEL('OLS_DuAn') from dual;
+select SA_SESSION.ROW_LABEL('OLS_DuAn') from dual;
+select SA_SESSION.SA_USER_NAME('OLS_DuAn') from dual;
 
