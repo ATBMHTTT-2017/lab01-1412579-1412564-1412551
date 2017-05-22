590cd4fceff22366714ab82cca61b24d73783327
bichtramtran <bichtramtran.khtn@gmail.com>
2017-05-22 17:53:34 +0700

    Data test policy_[5]_[7]

---
+++ 1412579-data.sql
@@ -0,0 +1,188 @@
+-- NhanVien --
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Vũ Minh Trí','Q12, HCM','0973178325','itvmtri@gmail.com', null, null, 69696969,1);
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Vũ Thị Minh Trang','Q12, HCM','0909697895','vtmtrang@gmail.com', null, null, 9999999,0);
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Đặng Bảo Trang','Quận Thủ Đức, HCM','0167627489','dbtrang@gmail.com', null, null, 23698712,0);
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Nguyễn Đang Tích','Q2, HCM','01699423671','ndtich@gmail.com', null, null, 19999999,0);
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Trần Thuỳ Bích Trâm','Q12, HCM','0123456789','ttbtram@gmail.com', null, null, 9999999,0);
+
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Hoàng Anh Tú','Q1, HCM','01207567891','hatu@gmail.com', null, null, 199999999,0);
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Trần Trung Tính','HCM','0909525471','EMAIL_01@gmail.com', null, null, 50000000,1);
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Trần Gia Bảo','HCM','0972515674','EMAIL_02@gmail.com', null, null, 50000000,1);
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Phan Thị Minh Phượng','HCM','0902231446','EMAIL_03@gmail.com', null, null, 50000000,1);
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Phạm Quốc Toàn','HCM','0968719811','EMAIL_04@gmail.com', null, null, 50000000,1);
+
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Phạm Ngọc Trí','HCM','0902763251','EMAIL_05@gmail.com', null, null, 50000000,0);
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Hoàng Thị Bích Vân','HCM','01693471568','EMAIL_06@gmail.com', null, null, 50000000,0);
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Hoàng Nhật Sương','HCM','0967586231','EMAIL_07@gmail.com', null, null, 50000000,0);
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Ngô Thuỳ Tuyết Trinh','HCM','01203896715','EMAIL_08@gmail.com', null, null, 50000000,0);
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Nguyễn Thái Duy','HCM','0902996774','EMAIL_09@gmail.com', null, null, 50000000,0);
+
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Trần Mai Anh','HCM','0969751429','EMAIL_10@gmail.com', null, null, 50000000,0);
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Nguyễn Quốc Huy','HCM','0969553869','EMAIL_11@gmail.com', null, null, 50000000,0);
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Võ Hoài Sơn','HCM','0972576843','EMAIL_12@gmail.com', null, null, 50000000,0);
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Hoàng Trung','HCM','0975151236','EMAIL_13@gmail.com', null, null, 50000000,0);
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Vũ Trí','HCM','01693556872','EMAIL_14@gmail.com', null, null, 50000000,0);
+
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Cường Kenvin','HCM','01699751289','EMAIL_15@gmail.com', null, null, 50000000,0);
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Trần Trường Sinh','HCM','01207554662','EMAIL_11@gmail.com', null, null, 50000000,0);
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Lâm Hoàng','HCM','0969587741','EMAIL_12@gmail.com', null, null, 50000000,0);
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Tiêu Viêm','HCM','0909253674','EMAIL_13@gmail.com', null, null, 50000000,0);
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Lương Thanh Trúc','HCM','0902542649','EMAIL_14@gmail.com', null, null, 50000000,0);
+
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Nguyễn Thảo','HCM','0978368521','EMAIL_15@gmail.com', null, null, 50000000,0);
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Lang Thang','HCM','0909511236','EMAIL_11@gmail.com', null, null, 50000000,0);
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Lâm Lang Thiên','HCM','0972587432','EMAIL_11@gmail.com', null, null, 50000000,0);
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Liên Đới','HCM','0969546897','EMAIL_12@gmail.com', null, null, 50000000,0);
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Tuỳ Tùng','HCM','0902743689','EMAIL_13@gmail.com', null, null, 50000000,0);
+
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Kha Ni Ni','HCM','0975741964','EMAIL_14@gmail.com', null, null, 50000000,0);
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Ngự Không','HCM','01207458962','EMAIL_15@gmail.com', null, null, 50000000,0);
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Lương Thanh Đoàn','HCM','01697456875','EMAIL_11@gmail.com', null, null, 50000000,0);
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Tần Chính','HCM','01699886772','EMAIL_12@gmail.com', null, null, 50000000,0);
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Băng Sương','HCM','0909547123','EMAIL_13@gmail.com', null, null, 50000000,0);
+
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Hầu Phí','HCM','0903754821','EMAIL_14@gmail.com', null, null, 50000000,0);
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Hắc Diện','HCM','0967412687','EMAIL_15@gmail.com', null, null, 50000000,0);
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Tang Đồ','HCM','01699751459','EMAIL_11@gmail.com', null, null, 50000000,0);
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Phan Phước Trí','HCM','01699558776','EMAIL_12@gmail.com', null, null, 50000000,0);
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Nguyễn Duy','HCM','0902547896','EMAIL_13@gmail.com', null, null, 50000000,0);
+
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Phan Thị Nghĩa','HCM','0973751698','EMAIL_14@gmail.com', null, null, 50000000,0);
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Trang Sĩ Dũ','HCM','0902556881','EMAIL_15@gmail.com', null, null, 50000000,0);
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Trần Nhật Anh','HCM','0972556883','EMAIL_11@gmail.com', null, null, 50000000,0);
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Hoàng Anh Thi Nguyễn','HCM','0969885226','EMAIL_12@gmail.com', null, null, 50000000,0);
+INSERT INTO NHANVIEN(HOTEN, DIACHI, DIENTHOAI, EMAIL, MAPHONG, CHINHANH, LUONG,CHUCVU) VALUES('Đồng Hữu Cơ','HCM','0975881223','EMAIL_13@gmail.com', null, null, 50000000,0);
+
+-- Update chiNhanh bảng NhanVien --
+update NhanVien set chiNhanh = 1 where maNV = 10000;
+update NhanVien set chiNhanh = 1 where maNV = 10001;
+update NhanVien set chiNhanh = 2 where maNV = 10002;
+update NhanVien set chiNhanh = 3 where maNV = 10003;
+update NhanVien set chiNhanh = 4 where maNV = 10004;
+update NhanVien set chiNhanh = 5 where maNV = 10005;
+update NhanVien set chiNhanh = 6 where maNV = 10006;
+update NhanVien set chiNhanh = 7 where maNV = 10007;
+update NhanVien set chiNhanh = 2 where maNV = 10008;
+update NhanVien set chiNhanh = 3 where maNV = 10009;
+update NhanVien set chiNhanh = 4 where maNV = 10010;
+update NhanVien set chiNhanh = 5 where maNV = 10011;
+update NhanVien set chiNhanh = 6 where maNV = 10012;
+update NhanVien set chiNhanh = 7 where maNV = 10013;
+update NhanVien set chiNhanh = 1 where maNV = 10014;
+update NhanVien set chiNhanh = 2 where maNV = 10015;
+update NhanVien set chiNhanh = 3 where maNV = 10016;
+update NhanVien set chiNhanh = 4 where maNV = 10017;
+update NhanVien set chiNhanh = 5 where maNV = 10018;
+update NhanVien set chiNhanh = 6 where maNV = 10019;
+update NhanVien set chiNhanh = 7 where maNV = 10020;
+update NhanVien set chiNhanh = 1 where maNV = 10021;
+update NhanVien set chiNhanh = 2 where maNV = 10022;
+update NhanVien set chiNhanh = 3 where maNV = 10023;
+update NhanVien set chiNhanh = 4 where maNV = 10024;
+update NhanVien set chiNhanh = 5 where maNV = 10025;
+update NhanVien set chiNhanh = 6 where maNV = 10026;
+update NhanVien set chiNhanh = 7 where maNV = 10027;
+update NhanVien set chiNhanh = 1 where maNV = 10028;
+update NhanVien set chiNhanh = 2 where maNV = 10029;
+update NhanVien set chiNhanh = 3 where maNV = 10030;
+update NhanVien set chiNhanh = 4 where maNV = 10031;
+update NhanVien set chiNhanh = 5 where maNV = 10032;
+update NhanVien set chiNhanh = 6 where maNV = 10033;
+update NhanVien set chiNhanh = 7 where maNV = 10034;
+update NhanVien set chiNhanh = 1 where maNV = 10035;
+update NhanVien set chiNhanh = 2 where maNV = 10036;
+update NhanVien set chiNhanh = 3 where maNV = 10037;
+update NhanVien set chiNhanh = 4 where maNV = 10038;
+update NhanVien set chiNhanh = 5 where maNV = 10039;
+update NhanVien set chiNhanh = 6 where maNV = 10040;
+update NhanVien set chiNhanh = 7 where maNV = 10041;
+update NhanVien set chiNhanh = 1 where maNV = 10042;
+update NhanVien set chiNhanh = 2 where maNV = 10043;
+update NhanVien set chiNhanh = 3 where maNV = 10044;
+
+-- Update maPhong bảng NhanVien --
+update NhanVien set maPhong = 1 where maNV = 10000;
+update NhanVien set maPhong = 1 where maNV = 10001;
+update NhanVien set maPhong = 2 where maNV = 10002;
+update NhanVien set maPhong = 3 where maNV = 10003;
+update NhanVien set maPhong = 4 where maNV = 10004;
+update NhanVien set maPhong = 5 where maNV = 10005;
+update NhanVien set maPhong = 6 where maNV = 10006;
+update NhanVien set maPhong = 7 where maNV = 10007;
+update NhanVien set maPhong = 8 where maNV = 10008;
+update NhanVien set maPhong = 1 where maNV = 10009;
+update NhanVien set maPhong = 2 where maNV = 10010;
+update NhanVien set maPhong = 3 where maNV = 10011;
+update NhanVien set maPhong = 4 where maNV = 10012;
+update NhanVien set maPhong = 5 where maNV = 10013;
+update NhanVien set maPhong = 6 where maNV = 10014;
+update NhanVien set maPhong = 7 where maNV = 10015;
+update NhanVien set maPhong = 8 where maNV = 10016;
+update NhanVien set maPhong = 1 where maNV = 10017;
+update NhanVien set maPhong = 2 where maNV = 10018;
+update NhanVien set maPhong = 3 where maNV = 10019;
+update NhanVien set maPhong = 4 where maNV = 10020;
+update NhanVien set maPhong = 5 where maNV = 10021;
+update NhanVien set maPhong = 6 where maNV = 10022;
+update NhanVien set maPhong = 7 where maNV = 10023;
+update NhanVien set maPhong = 8 where maNV = 10024;
+update NhanVien set maPhong = 1 where maNV = 10025;
+update NhanVien set maPhong = 2 where maNV = 10026;
+update NhanVien set maPhong = 3 where maNV = 10027;
+update NhanVien set maPhong = 4 where maNV = 10028;
+update NhanVien set maPhong = 5 where maNV = 10029;
+update NhanVien set maPhong = 6 where maNV = 10030;
+update NhanVien set maPhong = 7 where maNV = 10031;
+update NhanVien set maPhong = 8 where maNV = 10032;
+update NhanVien set maPhong = 1 where maNV = 10033;
+update NhanVien set maPhong = 2 where maNV = 10034;
+update NhanVien set maPhong = 3 where maNV = 10035;
+update NhanVien set maPhong = 4 where maNV = 10036;
+update NhanVien set maPhong = 5 where maNV = 10037;
+update NhanVien set maPhong = 6 where maNV = 10038;
+update NhanVien set maPhong = 7 where maNV = 10039;
+update NhanVien set maPhong = 8 where maNV = 10040;
+update NhanVien set maPhong = 1 where maNV = 10041;
+update NhanVien set maPhong = 2 where maNV = 10042;
+update NhanVien set maPhong = 3 where maNV = 10043;
+update NhanVien set maPhong = 4 where maNV = 10044;
+
+-- ChiNhanh --
+INSERT INTO CHINHANH(TENCN,TRUONGCHINHANH) VALUES ('TPHCM',10021);
+INSERT INTO CHINHANH(TENCN,TRUONGCHINHANH) VALUES ('Hà Nội',10022);
+INSERT INTO CHINHANH(TENCN,TRUONGCHINHANH) VALUES ('Đà Nẵng',10023);
+INSERT INTO CHINHANH(TENCN,TRUONGCHINHANH) VALUES ('Vũng Tàu',10024);
+INSERT INTO CHINHANH(TENCN,TRUONGCHINHANH) VALUES ('Đồng Nai',10025);
+INSERT INTO CHINHANH(TENCN,TRUONGCHINHANH) VALUES ('Seoul',10026);
+INSERT INTO CHINHANH(TENCN,TRUONGCHINHANH) VALUES ('Hồng Kông',10027);
+
+-- PhongBan --
+INSERT INTO PHONGBAN(TENPHONG,TRUONGPHONG,NGAYNHANCHUC,SONHANVIEN,CHINHANH) VALUES ('Giám đốc',10001,TO_DATE('2017/01/01','yyyy/mm/dd'),5,1);
+INSERT INTO PHONGBAN(TENPHONG,TRUONGPHONG,NGAYNHANCHUC,SONHANVIEN,CHINHANH) VALUES ('Nhân sự',10002,TO_DATE('2017/01/01','yyyy/mm/dd'),10,2);
+INSERT INTO PHONGBAN(TENPHONG,TRUONGPHONG,NGAYNHANCHUC,SONHANVIEN,CHINHANH) VALUES ('Kế toán',10003,TO_DATE('2017/01/01','yyyy/mm/dd'),5,3);
+INSERT INTO PHONGBAN(TENPHONG,TRUONGPHONG,NGAYNHANCHUC,SONHANVIEN,CHINHANH) VALUES ('Kế hoạch',10004,TO_DATE('2017/01/01','yyyy/mm/dd'),5,4);
+INSERT INTO PHONGBAN(TENPHONG,TRUONGPHONG,NGAYNHANCHUC,SONHANVIEN,CHINHANH) VALUES ('Nghiên cứu',10005,TO_DATE('2017/01/01','yyyy/mm/dd'),6,5);
+INSERT INTO PHONGBAN(TENPHONG,TRUONGPHONG,NGAYNHANCHUC,SONHANVIEN,CHINHANH) VALUES ('Kinh doanh',10030,TO_DATE('2017/01/01','yyyy/mm/dd'),10,6);
+INSERT INTO PHONGBAN(TENPHONG,TRUONGPHONG,NGAYNHANCHUC,SONHANVIEN,CHINHANH) VALUES ('Tiếp thị',10031,TO_DATE('2017/01/01','yyyy/mm/dd'),8,7);
+INSERT INTO PHONGBAN(TENPHONG,TRUONGPHONG,NGAYNHANCHUC,SONHANVIEN,CHINHANH) VALUES ('Giám sát',10032,TO_DATE('2017/01/01','yyyy/mm/dd'),6,2);
+
+-- DuAn --
+INSERT INTO DUAN(TENDA,KINHPHI,PHONGCHUTRI,TRUONGDA) VALUES ('Tạo đột biến gen ở loài vật',99999999999,1,10010);
+INSERT INTO DUAN(TENDA,KINHPHI,PHONGCHUTRI,TRUONGDA) VALUES ('Tạo đột biến gen ở loài chó',999999342399,2,10011);
+INSERT INTO DUAN(TENDA,KINHPHI,PHONGCHUTRI,TRUONGDA) VALUES ('Tạo đột biến gen ở loài khỉ',999923499,3,10012);
+INSERT INTO DUAN(TENDA,KINHPHI,PHONGCHUTRI,TRUONGDA) VALUES ('Tạo đột biến gen ở loài dơi',99912312999,4,10013);
+INSERT INTO DUAN(TENDA,KINHPHI,PHONGCHUTRI,TRUONGDA) VALUES ('Kết hợp gen của người và alien',9945645654999,5,10014);
+
+-- PhanCong --
+INSERT INTO PHANCONG(MaNV,DUAN,VAITRO,PHUCAP) values(10015,1,'Nghiên cứu sinh',1000000);
+INSERT INTO PHANCONG(MaNV,DUAN,VAITRO,PHUCAP) values(10016,2,'Giáo sư',5000000);
+INSERT INTO PHANCONG(MaNV,DUAN,VAITRO,PHUCAP) values(10017,3,'Tiến sĩ',01100000);
+INSERT INTO PHANCONG(MaNV,DUAN,VAITRO,PHUCAP) values(10018,4,'Trưởng dự án',8000000);
+INSERT INTO PHANCONG(MaNV,DUAN,VAITRO,PHUCAP) values(10019,5,'Phụ tá',900000);
+
+-- ChiTieu --
+INSERT INTO CHITIEU(TENCHITIEU,SOTIEN,DUAN) VALUES('Top 1 thế giới',9999999999,1);
+INSERT INTO CHITIEU(TENCHITIEU,SOTIEN,DUAN) VALUES('Top 2 thế giới',5654646,2);
+INSERT INTO CHITIEU(TENCHITIEU,SOTIEN,DUAN) VALUES('Top 3 thế giới',543634564,3);
+INSERT INTO CHITIEU(TENCHITIEU,SOTIEN,DUAN) VALUES('Top 4 thế giới',45362345,4);
+INSERT INTO CHITIEU(TENCHITIEU,SOTIEN,DUAN) VALUES('Top 5 thế giới',100098766,5);
