61c06a1856ea64cc72cc041c9a6d25241424af18
bichtramtran <bichtramtran.khtn@gmail.com>
2017-05-22 17:49:46 +0700

    Update policy_[2]_[1412579].sql

--- policy_[2]_[1412579].sql
+++ policy_[2]_[1412579].sql
@@ -8,8 +8,19 @@ CREATE ROLE TruongDuAn IDENTIFIED BY NeuCan;
 --Grant các quyền cho Role
 --Trưởng phòng
 GRANT INSERT, UPDATE ON DUAN TO TruongPhong;
+GRANT EXECUTE ON capNhatPhongBan_TruongPhong TO TruongPhong;
+--Trưởng chi nhánh
+GRANT EXECUTE ON capNhatPhongBan_TruongCN TO TruongChiNhanh;
+
 
 GRANT SELECT ON DUAN TO GiamDoc
 GRANT SELECT ON PHONGBAN TO GiamDoc
 GRANT SELECT ON CHINHANH TO GiamDoc
 GRANT SELECT ON CHITIEU TO GiamDoc
+
+-- Grant role cho user
+GRANT TruongPhong to "10001", "10002", "10003", "10004", "10005", "10030", "10031", "10032";
+GRANT GiamDoc to "10000", "10006", "10007", "10008", "10009";
+GRANT TruongChiNhanh to "10021", "10022", "10023", "10024", "10025";
+GRANT NhanVien to "10016", "10017", "10018", "10019", "10020";
+GRANT TruongDuAn to "10010", "10011", "10012", "10013", "10014";
